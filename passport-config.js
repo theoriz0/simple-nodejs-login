7b1394b42a4954be6e0e4420d12022dd61dde2b5
zhang00710 <zhang_00710@sina.com>
2021-04-27 09:41:04 +0800

    initial commit, finish frontend pages

---
+++ passport-config.js
@@ -0,0 +1,26 @@
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcrypt')
+
+export default function initialize(passport) {
+  const authenticateUser = (email, password, done) => {
+    const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, {
+        message: 'No user with given email'
+      })
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, {
+          message: 'Incorrect password'
+        })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
+  passport.use(new LocalStrategy( { usernameField: 'email'}), authenticateUser)
+}
